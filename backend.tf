56424daf44705e95d731df642c87777d3fe0a3cb
Munara241 <153228849+Munara241@users.noreply.github.com>
2024-07-06 13:25:04 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
    backend "s3" {
      bucket = "jenkins-kaizen"
-     key = "${var.region}/${var.az}/terraform.tfstate"
+     key = "terraform.tfstate"
      region = "us-east-2"
    }
 }
